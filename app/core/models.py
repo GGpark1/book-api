5890acabd245871eb9a4399592d546a0d4239d81
GGpark1 <doempgg@gmail.com>
2023-02-10 11:48:33 +0900

    added User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+"""
+Database models.
+"""
+import re
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **kwargs):
+        """Create, save and return a new user."""
+
+        p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$') # noqa
+
+        if not p.match(email):
+            raise ValueError('Check your email is valid.')
+
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """USer in the system."""
+
+    class Gender(models.IntegerChoices):
+        NONE = 0
+        MALE = 1
+        FEMALE = 2
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    gender = models.IntegerField(choices=Gender.choices)
+    birth = models.DateField(null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,11 +17,54 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'password123'
-        payload = {'gender': 'male',
-                   'birth': 900410,}
+        payload = {'gender': 0,
+                   'birth': '1990-04-10',}
         user = get_user_model().objects.create_user(email=email,
                                                     password=password,
                                                     **payload)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+
+        payload = {'gender': 0,
+                   'birth': '1990-04-10',}
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='sample123',
+                **payload
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_is_user_email_valid(self):
+        """Test email format is valid"""
+        sample_emails = [
+            '@example.com',
+            'test1@com',
+            'example.com',
+            'test1',
+            '.com',
+            '',
+        ]
+
+        payload = {'gender': 0,
+                   'birth': '1990-04-10',}
+
+        for email in sample_emails:
+            with self.assertRaises(ValueError):
+                get_user_model().objects.create_user(
+                    email=email,
+                    password='sample123',
+                    **payload
+                )
+
